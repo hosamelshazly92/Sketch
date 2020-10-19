e84d5cd4d8a0e043db498ec23f30a1ed0f71cfe9
hosamelshazly92 <hosamelshazly92@gmail.com>
2020-10-19 00:02:18 -0700

    add capitalize first letter method my-command.js script display_message

--- display_message/src/my-command.js
+++ display_message/src/my-command.js
@@ -11,10 +11,23 @@ export default function() {
     (err, value) => {
       if (err) {
         // most likely the user canceled the input
+        sketch.UI.message(`No input was given`);
         return
       } else {
         // sketch.UI.message(`Welcome ${value}!`);
-        sketch.UI.alert('Greeting Message', `Welcome ${value}!`);
+        // sketch.UI.alert('Greeting Message', `Welcome ${value}!`);
+
+        let arr = value.split(" ");
+
+        let newArr = arr.map(myFunction);
+
+        function myFunction(word) {
+          return word.charAt(0).toUpperCase() + word.slice(1);
+        }
+
+        let newName = newArr.join(" ");
+
+        sketch.UI.alert('Greeting Message', `Welcome ${newName}!`);
       }
     }
   );
