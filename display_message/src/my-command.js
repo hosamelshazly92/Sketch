5eec08115c5f3ee3b47df398474a74f368ba3085
hosamelshazly92 <hosamelshazly92@gmail.com>
2020-10-19 02:11:45 -0700

    add icon display_message

--- display_message/src/my-command.js
+++ display_message/src/my-command.js
@@ -11,25 +11,18 @@ export default function() {
     (err, value) => {
       if (err) {
         // most likely the user canceled the input
-        sketch.UI.message(`No input was given`);
-        return
+        sketch.UI.message(`Sorry nothing to display, no input was given`);
       } else {
         // sketch.UI.message(`Welcome ${value}!`);
         // sketch.UI.alert('Greeting Message', `Welcome ${value}!`);
-
         let arr = value.split(" ");
-
         let newArr = arr.map(myFunction);
-
         function myFunction(word) {
           return word.charAt(0).toUpperCase() + word.slice(1);
         }
-
         let newName = newArr.join(" ");
-
-        sketch.UI.alert('Greeting Message', `Welcome ${newName}!`);
+        sketch.UI.alert('Greeting Message', `Welcome ${newName}!`); 
       }
     }
   );
-
 }
