718935066b78a5c0b75726c91f5076713185b04a
hosamelshazly92 <hosamelshazly92@gmail.com>
2020-10-18 21:43:01 -0700

    alert user input my-command.js script display_message

--- display_message/src/my-command.js
+++ display_message/src/my-command.js
@@ -2,5 +2,21 @@ import sketch from 'sketch'
 // documentation: https://developer.sketchapp.com/reference/api/
 
 export default function() {
-  sketch.UI.message("It's alive 🙌")
+  
+  sketch.UI.getInputFromUser(
+    "What's your name?",
+    {
+      initialValue: 'Type your name here....',
+    },
+    (err, value) => {
+      if (err) {
+        // most likely the user canceled the input
+        return
+      } else {
+        // sketch.UI.message(`Welcome ${value}!`);
+        sketch.UI.alert('Greeting Message', `Welcome ${value}!`);
+      }
+    }
+  );
+
 }
