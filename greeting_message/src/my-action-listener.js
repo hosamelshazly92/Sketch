b77c3c24f8803367ddcff70a9d19a595a75318eb
hosamelshazly92 <hosamelshazly92@gmail.com>
2020-10-21 04:37:35 -0700

    recreate greeting_message

--- greeting_message/src/my-action-listener.js
+++
@@ -1,7 +0,0 @@
-import sketch from 'sketch'
-// documentation: https://developer.sketchapp.com/reference/api/
-
-export default function onOpenDocument(context) {
-  // context.actionContext.document.showMessage('Document Opened')
-  sketch.UI.message("It's alive 🙌")
-}
