573c7e3935aaf890c414c469d51bebc885e4e43c
hosamelshazly92 <hosamelshazly92@gmail.com>
2020-10-21 22:05:11 -0700

    recreate greeting_message & install dependencies

--- greeting_message/src/my-command.js
+++ greeting_message/src/my-command.js
@@ -1,6 +1,13 @@
-import sketch from 'sketch'
+// import sketch from 'sketch'
 // documentation: https://developer.sketchapp.com/reference/api/
 
-export default function() {
-  sketch.UI.message("It's alive 🙌")
+// export default function() {
+//   sketch.UI.message("It's alive 🙌")
+// }
+
+const sketch = require('sketch')
+const { randomEmoji } = require('./shared')
+
+export default function onRun(context) {
+  sketch.UI.message(`Hello Sketch ${randomEmoji()}`)
 }
