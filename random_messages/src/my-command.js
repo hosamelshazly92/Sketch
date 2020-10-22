6a3296636a7c6388558fcb674d1346bba489b821
hosamelshazly92 <hosamelshazly92@gmail.com>
2020-10-21 23:49:20 -0700

    create shared.js script random_messages

--- random_messages/src/my-command.js
+++ random_messages/src/my-command.js
@@ -1,6 +1,6 @@
-import sketch from 'sketch'
-// documentation: https://developer.sketchapp.com/reference/api/
+const sketch = require('sketch')
+const { randomEmoji } = require('./shared')
 
-export default function() {
-  sketch.UI.message("It's alive 🙌")
+export default function onRun(context) {
+  sketch.UI.message(`Hello Sketch ${randomEmoji()}`)
 }
