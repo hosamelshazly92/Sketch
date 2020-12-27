ff747c0d30e65e196e19b66e8e3f3a2fe9277e38
hosamelshazly92 <hosamelshazly92@gmail.com>
2020-12-26 16:26:33 -0800

    edit src/my-command.js & src/manifest.json

--- alert/src/my-command.js
+++ alert/src/my-command.js
@@ -2,5 +2,27 @@ import sketch from 'sketch'
 // documentation: https://developer.sketchapp.com/reference/api/
 
 export default function() {
-  sketch.UI.message("It's alive 🙌")
+  
+  sketch.UI.getInputFromUser(
+    "Type your name here....",
+    {
+      initialValue: 'Name',
+    },
+    (err, value) => {
+      if (err) {
+        // most likely the user canceled the input
+        sketch.UI.message(`Sorry, no input was given`);
+      } else {
+        // sketch.UI.message(`Welcome ${value}!`);
+        // sketch.UI.alert('Greeting Message', `Welcome ${value}!`);
+        let arr = value.split(" ");
+        let newArr = arr.map(myFunction);
+        function myFunction(word) {
+          return word.charAt(0).toUpperCase() + word.slice(1);
+        }
+        let newName = newArr.join(" ");
+        sketch.UI.alert('Greetings', `Welcome ${newName}!`); 
+      }
+    }
+  );
 }
